2c46d6767e2cd4bb55bb48aa3a31bef548d2bf02
Rob Masters <rjcmasters@gmail.com>
2021-05-19 17:30:08 +0100

    successfully rendered an image

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,45 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
+use std::env;
+use std::path::Path;
 
-fn main() -> Result<(), String> {
+fn render(texture: &Texture, canvas: &mut WindowCanvas, color: Color) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.copy(&texture, None, None)?;
+    canvas.present();
+
+    Ok(())
+}
+
+pub fn run(png: &Path) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem
+        .window("DinoScore", 800, 600)
         .position_centered()
         .build()
-        .expect("failed to initialise video subsytem");
+        .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas()
+    let mut canvas = window
+        .into_canvas()
+        .software()
         .build()
-        .expect("canvas generation failed");
+        .map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(png)?;
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Event processing
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -34,10 +49,26 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&texture, &mut canvas, Color::RGB(i, 64, 255 -i))?;
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: cargo run /path/to/image.(png|jpg)")
+    } else {
+        run(Path::new(&args[1]))?;
     }
 
     Ok(())
