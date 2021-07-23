bf63dca9ac902d1568973b076c940378fb1141bc
Rob Masters <rjcmasters@gmail.com>
2021-07-23 12:05:20 +0100

    bevy port begun; basic plane and a single spawned sphere

--- src/entities.rs
+++ src/entities.rs
@@ -1,133 +1,156 @@
-use nalgebra::{
-    base::Vector2,
-    vector,
-};
+use bevy::prelude::*;
 use rand::prelude::*;
-use specs::prelude::*;
 
 use crate::{
     components::*,
     constants,
 };
 
-pub fn setup_initial_entities(world: &mut World) {
-    let circle_qty = 0;
-    let polygon_qty = 0;
+// Plugin
+pub struct SpawnerPlugin;
 
-    let mut spawner = Spawner::new(world);
-
-    // spawn player.
-    spawner.spawn_player(0.0, 0.0, 10.0, 10.0);
-
-    // spawn world boundaries.
-    spawner.spawn_boundaries();
-
-    // spawn random circles.
-    let mut rng = thread_rng();
-    for _ in 0..circle_qty {
-        let radius = rng.gen_range(1.0..20.0);
-        let x = rng.gen_range(constants::FMIN_X + radius..constants::FMAX_X - radius);
-        let y = rng.gen_range(constants::FMIN_Y + radius..constants::FMAX_Y - radius);
-        let mass = radius * radius * radius * 0.01;
-        spawner.spawn_circle(x, y, radius, mass);
-    }
-
-    // spawn polygons
-    // TODO currently just a generic polygon.
-    let vertices = vec![
-        vector![-20.0, 10.0],
-        vector![0.0, 20.0],
-        vector![20.0, 0.0],
-        vector![10.0, -20.0],
-        vector![-10.0, -10.0]
-    ];
-
-    for _ in 0..polygon_qty {
-        spawner.spawn_polygon(50.0, 50.0, 100.0, vertices.clone());
+impl Plugin for SpawnerPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(spawn_sphere.system());
     }
 }
 
-struct Spawner<'a> {
-    world: &'a mut World,
+
+// Systems
+fn spawn_sphere(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let radius = 1.0;
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Icosphere { radius, subdivisions: 10 })),
+        material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+        transform: Transform::from_xyz(0.0, radius, 0.0),
+        ..Default::default()
+    });
 }
 
-impl<'a> Spawner<'a> {
-    pub fn new(world: &'a mut World) -> Self {
-        Self { world }
-    }
 
-    fn spawn_circle(&mut self, x_pos: f64, y_pos: f64, radius: f64, mass: f64) {
-        let mut rng = thread_rng();
-        self.world.create_entity()
-                  .with(Position { vector: vector![x_pos, y_pos] })
-                  // physics
-                  .with(Forces::default())
-                  .with(Mass { value: mass, inverse: 1.0 / mass })
-                  .with(Velocity {
-                      vector: vector![rng.gen_range(-10.0..10.0), rng.gen_range(-10.0..10.0)] })
-                  .with(CircleCollider::new(radius))
-                  // rendering
-                  .with(RenderableCircle::new(radius))
-                  .with(RenderColour::new(255, 210, 0))
-                  .build();
-    }
-
-    fn spawn_polygon(&mut self, x_pos: f64, y_pos: f64, mass: f64, vertices: Vec<Vector2<f64>>) {
-        let mut rng = thread_rng();
-        self.world.create_entity()
-                  .with(Position { vector: vector![x_pos, y_pos] })
-                  // physics
-                  .with(Forces::default())
-                  .with(Mass { value: mass, inverse: 1.0 / mass })
-                  .with(Velocity { vector: vector![rng.gen_range(-10.0..10.0), rng.gen_range(-10.0..10.0)] })
-                  .with(PolygonCollider::new(&vertices))
-                  // rendering
-                  .with(RenderablePolygon::new(&vertices))
-                  .with(RenderColour::new(255, 120, 0))
-                  .build();
-    }
-
-
-    fn spawn_player(&mut self, x_pos: f64, y_pos: f64, radius: f64, mass: f64) {
-        self.world.create_entity()
-                  .with(Player)
-                  .with(KeyboardControlled)
-                  .with(Position::new(vector![x_pos, y_pos]))
-                  // physics
-                  .with(Force::default())
-                  .with(Drag::default())
-                  .with(Gravity::default())
-                  .with(Thrust::default())
-                  //.with(Forces::default())
-                  .with(Mass::new(mass))
-                  .with(Velocity::default())
-                  //.with(CircleCollider::new(radius))
-                  // rendering
-                  .with(RenderableCircle::new(radius))
-                  .with(RenderColour::new(0, 255, 0))
-                  .build();
-    }
-
-    fn spawn_boundaries(&mut self) {
-        let planes = vec![
-            // Top
-            (vector![0.0, 1.0], vector![constants::FMIN_X, constants::FMIN_Y]),
-            // Bottom
-            (vector![0.0, -1.0], vector![constants::FMAX_X, constants::FMAX_Y]),
-            // Left
-            (vector![1.0, 0.0], vector![constants::FMIN_X, constants::FMIN_Y]),
-            // Right
-            (vector![-1.0, 0.0], vector![constants::FMAX_X, constants::FMAX_Y]),
-        ];
-
-        for (n, pos) in planes {
-            self.world.create_entity()
-                      .with(Position { vector: pos })
-                      // physics
-                      .with(Mass { value: f64::INFINITY, inverse: 0.0 })
-                      .with(Velocity { vector: vector![0.0, 0.0] })
-                      .with(BoundaryCollider::new(n))
-                      .build();
-        }
-    }
-}
+//pub fn setup_initial_entities(world: &mut World) {
+//    let circle_qty = 0;
+//    let polygon_qty = 0;
+//
+//    let mut spawner = Spawner::new(world);
+//
+//    // spawn player.
+//    spawner.spawn_player(0.0, 0.0, 10.0, 10.0);
+//
+//    // spawn world boundaries.
+//    spawner.spawn_boundaries();
+//
+//    // spawn random circles.
+//    let mut rng = thread_rng();
+//    for _ in 0..circle_qty {
+//        let radius = rng.gen_range(1.0..20.0);
+//        let x = rng.gen_range(constants::FMIN_X + radius..constants::FMAX_X - radius);
+//        let y = rng.gen_range(constants::FMIN_Y + radius..constants::FMAX_Y - radius);
+//        let mass = radius * radius * radius * 0.01;
+//        spawner.spawn_circle(x, y, radius, mass);
+//    }
+//
+//    // spawn polygons
+//    // TODO currently just a generic polygon.
+//    let vertices = vec![
+//        vector![-20.0, 10.0],
+//        vector![0.0, 20.0],
+//        vector![20.0, 0.0],
+//        vector![10.0, -20.0],
+//        vector![-10.0, -10.0]
+//    ];
+//
+//    for _ in 0..polygon_qty {
+//        spawner.spawn_polygon(50.0, 50.0, 100.0, vertices.clone());
+//    }
+//}
+//
+//struct Spawner<'a> {
+//    world: &'a mut World,
+//}
+//
+//impl<'a> Spawner<'a> {
+//    pub fn new(world: &'a mut World) -> Self {
+//        Self { world }
+//    }
+//
+//    fn spawn_circle(&mut self, x_pos: f64, y_pos: f64, radius: f64, mass: f64) {
+//        let mut rng = thread_rng();
+//        self.world.create_entity()
+//                  .with(Position { vector: vector![x_pos, y_pos] })
+//                  // physics
+//                  .with(Forces::default())
+//                  .with(Mass { value: mass, inverse: 1.0 / mass })
+//                  .with(Velocity {
+//                      vector: vector![rng.gen_range(-10.0..10.0), rng.gen_range(-10.0..10.0)] })
+//                  .with(CircleCollider::new(radius))
+//                  // rendering
+//                  .with(RenderableCircle::new(radius))
+//                  .with(RenderColour::new(255, 210, 0))
+//                  .build();
+//    }
+//
+//    fn spawn_polygon(&mut self, x_pos: f64, y_pos: f64, mass: f64, vertices: Vec<Vector2<f64>>) {
+//        let mut rng = thread_rng();
+//        self.world.create_entity()
+//                  .with(Position { vector: vector![x_pos, y_pos] })
+//                  // physics
+//                  .with(Forces::default())
+//                  .with(Mass { value: mass, inverse: 1.0 / mass })
+//                  .with(Velocity { vector: vector![rng.gen_range(-10.0..10.0), rng.gen_range(-10.0..10.0)] })
+//                  .with(PolygonCollider::new(&vertices))
+//                  // rendering
+//                  .with(RenderablePolygon::new(&vertices))
+//                  .with(RenderColour::new(255, 120, 0))
+//                  .build();
+//    }
+//
+//
+//    fn spawn_player(&mut self, x_pos: f64, y_pos: f64, radius: f64, mass: f64) {
+//        self.world.create_entity()
+//                  .with(Player)
+//                  .with(KeyboardControlled)
+//                  .with(Position::new(vector![x_pos, y_pos]))
+//                  // physics
+//                  .with(Force::default())
+//                  .with(Drag::default())
+//                  .with(Gravity::default())
+//                  .with(Thrust::default())
+//                  //.with(Forces::default())
+//                  .with(Mass::new(mass))
+//                  .with(Velocity::default())
+//                  //.with(CircleCollider::new(radius))
+//                  // rendering
+//                  .with(RenderableCircle::new(radius))
+//                  .with(RenderColour::new(0, 255, 0))
+//                  .build();
+//    }
+//
+//    fn spawn_boundaries(&mut self) {
+//        let planes = vec![
+//            // Top
+//            (vector![0.0, 1.0], vector![constants::FMIN_X, constants::FMIN_Y]),
+//            // Bottom
+//            (vector![0.0, -1.0], vector![constants::FMAX_X, constants::FMAX_Y]),
+//            // Left
+//            (vector![1.0, 0.0], vector![constants::FMIN_X, constants::FMIN_Y]),
+//            // Right
+//            (vector![-1.0, 0.0], vector![constants::FMAX_X, constants::FMAX_Y]),
+//        ];
+//
+//        for (n, pos) in planes {
+//            self.world.create_entity()
+//                      .with(Position { vector: pos })
+//                      // physics
+//                      .with(Mass { value: f64::INFINITY, inverse: 0.0 })
+//                      .with(Velocity { vector: vector![0.0, 0.0] })
+//                      .with(BoundaryCollider::new(n))
+//                      .build();
+//        }
+//    }
+//}
--- src/runner.rs
+++ src/runner.rs
@@ -1,125 +1,186 @@
-use sdl2::{
-    image::{InitFlag, LoadTexture},
-    pixels::Color,
+use bevy::{
+    prelude::*,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
 };
-use specs::prelude::*;
 
 use crate::{
     //animator::Animator,
     constants,
-    entities,
-    event_processor,
-    keyboard::Keyboard,
-    physics::{
-        collision_detection::CollisionDetectionSys,
-        collision_response::CollisionResponseSys,
-        forces::ForceSys,
-        integrator::IntegrationSys,
-    },
-    renderer,
-    resources::{DeltaTime, MovementCommandStack},
-    timing::Timing,
+    entities::SpawnerPlugin,
+    //event_processor,
+    //keyboard::Keyboard,
+    //physics::{
+        //collision_detection::CollisionDetectionSys,
+        //collision_response::CollisionResponseSys,
+        //forces::ForceSys,
+        //integrator::IntegrationSys,
+    //},
+    //renderer,
+    //resources::{DeltaTime, MovementCommandStack},
+    //timing::Timing,
 };
 
 pub fn run() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
-
-    let window = video_subsystem
-        .window("Physics Simulation", constants::SCREEN_WIDTH, constants::SCREEN_HEIGHT)
-        .position_centered()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let texture_creator = canvas.texture_creator();
-    let textures = [
-        texture_creator.load_texture("assets/females/F_01.png")?,
-        texture_creator.load_texture("assets/females/F_02.png")?,
-        texture_creator.load_texture("assets/females/F_03.png")?,
-        texture_creator.load_texture("assets/females/F_04.png")?,
-        texture_creator.load_texture("assets/females/F_05.png")?,
-        texture_creator.load_texture("assets/females/F_06.png")?,
-        texture_creator.load_texture("assets/females/F_07.png")?,
-        texture_creator.load_texture("assets/females/F_08.png")?,
-        texture_creator.load_texture("assets/females/F_09.png")?,
-        texture_creator.load_texture("assets/females/F_10.png")?,
-        texture_creator.load_texture("assets/females/F_11.png")?,
-        texture_creator.load_texture("assets/females/F_12.png")?,
-        texture_creator.load_texture("assets/males/M_01.png")?,
-        texture_creator.load_texture("assets/males/M_02.png")?,
-        texture_creator.load_texture("assets/males/M_03.png")?,
-        texture_creator.load_texture("assets/males/M_04.png")?,
-        texture_creator.load_texture("assets/males/M_05.png")?,
-        texture_creator.load_texture("assets/males/M_06.png")?,
-        texture_creator.load_texture("assets/males/M_07.png")?,
-        texture_creator.load_texture("assets/males/M_08.png")?,
-        texture_creator.load_texture("assets/males/M_09.png")?,
-        texture_creator.load_texture("assets/males/M_10.png")?,
-        texture_creator.load_texture("assets/males/M_11.png")?,
-        texture_creator.load_texture("assets/males/M_12.png")?,
-    ];
-
-    let mut world = World::new();
-
-    // dispatcher 
-    let mut dispatcher = DispatcherBuilder::new()
-        .with(Timing, "Timing", &[])
-        .with(Keyboard, "Keyboard", &["Timing"])
-        .with(ForceSys, "Forces", &["Keyboard", "Timing"])
-        .with(IntegrationSys, "Integrator", &["Forces"])
-        .with(CollisionDetectionSys::new(), "CollisionDetection", &["Keyboard", "Timing"])
-        .with(CollisionResponseSys, "CollisionResponse", &["CollisionDetection", "Forces", "Keyboard", "Timing"])
-        .build();
-
-//    let mut dispatcher = DispatcherBuilder::new()
-//        .with(Timing, "Timing", &[])
-//        .with(Keyboard, "Keyboard", &["Timing"])
-//        .with(CollisionDetectionSys::new(), "CollisionDetection", &["Keyboard", "Timing"])
-//        .with(ForceSys, "ExternalForces", &["CollisionDetection", "Keyboard", "Timing"])
-//        .with(CollisionResponseSys, "CollisionResponse", &["CollisionDetection", "ExternalForces", "Keyboard", "Timing"])
-//        .with(MovementSys, "Movement", &["CollisionDetection", "CollisionResponse", "ExternalForces", "Keyboard", "Timing"])
-//        //.with(Animator, "Animator", &["Keyboard", "Timing"])
-//        .build();
-
-    dispatcher.setup(&mut world);
-
-    // Not threadsafe, so rendering system must be independently setup and not added to the dispatcher
-    renderer::SystemData::setup(&mut world);
-
-    // initialise resources
-    let movement_command_queue: MovementCommandStack = MovementCommandStack::new();
-    let delta_time = DeltaTime::new();
-
-    world.insert(movement_command_queue);
-    world.insert(delta_time);
-
-    // entities
-    entities::setup_initial_entities(&mut world);
-
-    // game loop
-    let mut event_pump = sdl_context.event_pump()?;
-    let i = 0;
-    let mut exit;
-    'running: loop {
-        // Event processing
-        exit = event_processor::process_events(&world, &mut event_pump);
-        if exit { break 'running };
-
-        // Update
-        //i = (i + 1) % 255; // update background color
-        let background_colour = Color::RGB(i, 64, 255 -i);
-        dispatcher.dispatch(&world);
-        world.maintain();
-
-        // Render
-        renderer::render(&mut canvas, background_colour, &textures, world.system_data())?;
-    }
+    App::build()
+        // plugins
+        .add_plugins(DefaultPlugins)
+        // --Debugging
+        .add_plugin(LogDiagnosticsPlugin::default())
+        //.add_plugin(FrameTimeDiagnosticsPlugin::default())
+        // --Custom
+        .add_plugin(SpawnerPlugin)
+        // resources
+        .insert_resource(Msaa { samples: 1 })
+        .insert_resource(WindowDescriptor {
+            title: "Physics Simulation".to_string(),
+            width: constants::SCREEN_WIDTH as f32,
+            height: constants::SCREEN_HEIGHT as f32,
+            resizable: false,
+            ..Default::default()
+        })
+        // events
+        // startup systems
+        .add_startup_system(setup.system())
+        // systems
+        // systems used during development
+        .add_system(bevy::input::system::exit_on_esc_system.system())
+        .add_system(bevy::window::exit_on_window_close_system.system())
+        .run();
 
     Ok(())
 }
+
+// Systems
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 60.0 })),
+        material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
+        ..Default::default()
+    });
+
+    // Camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(0.0, 60.0, 90.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    // Light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 15.0, 0.0)),
+        light: Light {
+            range: 150.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
+
+// TODO old code
+//pub fn _old_run() -> Result<(), String> {
+//    let sdl_context = sdl2::init()?;
+//    let video_subsystem = sdl_context.video()?;
+//    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+//
+//    let window = video_subsystem
+//        .window("Physics Simulation", constants::SCREEN_WIDTH, constants::SCREEN_HEIGHT)
+//        .position_centered()
+//        .build()
+//        .map_err(|e| e.to_string())?;
+//
+//    let mut canvas = window
+//        .into_canvas()
+//        .build()
+//        .map_err(|e| e.to_string())?;
+//
+//    let texture_creator = canvas.texture_creator();
+//    let textures = [
+//        texture_creator.load_texture("assets/females/F_01.png")?,
+//        texture_creator.load_texture("assets/females/F_02.png")?,
+//        texture_creator.load_texture("assets/females/F_03.png")?,
+//        texture_creator.load_texture("assets/females/F_04.png")?,
+//        texture_creator.load_texture("assets/females/F_05.png")?,
+//        texture_creator.load_texture("assets/females/F_06.png")?,
+//        texture_creator.load_texture("assets/females/F_07.png")?,
+//        texture_creator.load_texture("assets/females/F_08.png")?,
+//        texture_creator.load_texture("assets/females/F_09.png")?,
+//        texture_creator.load_texture("assets/females/F_10.png")?,
+//        texture_creator.load_texture("assets/females/F_11.png")?,
+//        texture_creator.load_texture("assets/females/F_12.png")?,
+//        texture_creator.load_texture("assets/males/M_01.png")?,
+//        texture_creator.load_texture("assets/males/M_02.png")?,
+//        texture_creator.load_texture("assets/males/M_03.png")?,
+//        texture_creator.load_texture("assets/males/M_04.png")?,
+//        texture_creator.load_texture("assets/males/M_05.png")?,
+//        texture_creator.load_texture("assets/males/M_06.png")?,
+//        texture_creator.load_texture("assets/males/M_07.png")?,
+//        texture_creator.load_texture("assets/males/M_08.png")?,
+//        texture_creator.load_texture("assets/males/M_09.png")?,
+//        texture_creator.load_texture("assets/males/M_10.png")?,
+//        texture_creator.load_texture("assets/males/M_11.png")?,
+//        texture_creator.load_texture("assets/males/M_12.png")?,
+//    ];
+//
+//    let mut world = specs::prelude::World::new();
+//
+//    // dispatcher 
+//    let mut dispatcher = specs::prelude::DispatcherBuilder::new()
+//        .with(Timing, "Timing", &[])
+//        .with(Keyboard, "Keyboard", &["Timing"])
+//        .with(ForceSys, "Forces", &["Keyboard", "Timing"])
+//        .with(IntegrationSys, "Integrator", &["Forces"])
+//        .with(CollisionDetectionSys::new(), "CollisionDetection", &["Keyboard", "Timing"])
+//        .with(CollisionResponseSys, "CollisionResponse", &["CollisionDetection", "Forces", "Keyboard", "Timing"])
+//        .build();
+//
+////    let mut dispatcher = DispatcherBuilder::new()
+////        .with(Timing, "Timing", &[])
+////        .with(Keyboard, "Keyboard", &["Timing"])
+////        .with(CollisionDetectionSys::new(), "CollisionDetection", &["Keyboard", "Timing"])
+////        .with(ForceSys, "ExternalForces", &["CollisionDetection", "Keyboard", "Timing"])
+////        .with(CollisionResponseSys, "CollisionResponse", &["CollisionDetection", "ExternalForces", "Keyboard", "Timing"])
+////        .with(MovementSys, "Movement", &["CollisionDetection", "CollisionResponse", "ExternalForces", "Keyboard", "Timing"])
+////        //.with(Animator, "Animator", &["Keyboard", "Timing"])
+////        .build();
+//
+//    dispatcher.setup(&mut world);
+//
+//    // Not threadsafe, so rendering system must be independently setup and not added to the dispatcher
+//    renderer::SystemData::setup(&mut world);
+//
+//    // initialise resources
+//    let movement_command_queue: MovementCommandStack = MovementCommandStack::new();
+//    let delta_time = DeltaTime::new();
+//
+//    world.insert(movement_command_queue);
+//    world.insert(delta_time);
+//
+//    // entities
+//    entities::setup_initial_entities(&mut world);
+//
+//    // game loop
+//    let mut event_pump = sdl_context.event_pump()?;
+//    let i = 0;
+//    let mut exit;
+//    'running: loop {
+//        // Event processing
+//        exit = event_processor::process_events(&world, &mut event_pump);
+//        if exit { break 'running };
+//
+//        // Update
+//        //i = (i + 1) % 255; // update background color
+//        let background_colour = Color::RGB(i, 64, 255 -i);
+//        dispatcher.dispatch(&world);
+//        world.maintain();
+//
+//        // Render
+//        renderer::render(&mut canvas, background_colour, &textures, world.system_data())?;
+//    }
+//
+//    Ok(())
+//}
