201ae932d9ba721bdf3c036fd2018d4e08e7d05a
Rob Masters <rjcmasters@gmail.com>
2021-09-08 13:17:08 +0100

    added more fields to the debug overlay and logging

--- src/debug/systems.rs
+++ src/debug/systems.rs
@@ -12,18 +12,22 @@ use crate::{
         PlayerDebugText,
     },
     physics::components::{
+        AngularVelocity,
         Drag,
+        Force,
         Impulse,
         ImpulsiveTorque,
         PhysTransform,
         Thrust,
+        Torque,
+        Velocity,
     },
     user_interaction::components::{
         Player,
     },
 };
 
-/// A SystemSet that overlays debugging information onto the screen.
+/// A SystemSet that overlays debugging information onto the screen and prints it to the console.
 pub fn get_system_set() -> SystemSet {
     SystemSet::new()
         .with_system(update_player_debug_text.system()
@@ -42,7 +46,7 @@ pub fn initialize(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let mut player_sections = vec![];
 
-    for _ in 0..10 {
+    for _ in 0..18 {
         player_sections.push(TextSection {
             value: "".to_string(),
             style: TextStyle {
@@ -57,8 +61,12 @@ pub fn initialize(mut commands: Commands, asset_server: Res<AssetServer>) {
     player_sections[0].value = "PLAYER DATA\n\nposition = ".to_string();
     player_sections[2].value = "\nthrust = ".to_string();
     player_sections[4].value = "\ndrag = ".to_string();
-    player_sections[6].value = "\nimpulse = ".to_string();
-    player_sections[8].value = "\nimpulsive_torque = ".to_string();
+    player_sections[6].value = "\nforces = ".to_string();
+    player_sections[8].value = "\ntorques = ".to_string();
+    player_sections[10].value = "\nvelocity = ".to_string();
+    player_sections[12].value = "\nangular velocity = ".to_string();
+    player_sections[14].value = "\nimpulse = ".to_string();
+    player_sections[16].value = "\nimpulsive_torque = ".to_string();
 
     commands
         .spawn_bundle(TextBundle {
@@ -123,25 +131,46 @@ pub fn initialize(mut commands: Commands, asset_server: Res<AssetServer>) {
 /// Populates the player section of text with relevant up-to-date values.
 fn update_player_debug_text(
     mut query: Query<&mut Text, With<PlayerDebugText>>,
-    player: Query<(&Drag, &PhysTransform, &Thrust), With<Player>>,
+    player: Query<(&AngularVelocity, &Drag, &Force, &PhysTransform, &Thrust, &Torque, &Velocity),
+        With<Player>>,
     player_optional: Query<(&Impulse, &ImpulsiveTorque), With<Player>>,
 ) {
     for mut text in query.iter_mut() {
-        let mut player_drag = DVec3::ZERO;
-        let mut player_impulse = DVec3::ZERO;
-        let mut player_impulsive_torque = DVec3::ZERO;
         let mut player_position = DVec3::ZERO;
         let mut player_thrust = DVec3::ZERO;
+        let mut player_drag = DVec3::ZERO;
+        let mut player_force = DVec3::ZERO;
+        let mut player_torque = DVec3::ZERO;
+        let mut player_velocity = DVec3::ZERO;
+        let mut player_angular_velocity = DVec3::ZERO;
+        let mut player_impulse = DVec3::ZERO;
+        let mut player_impulsive_torque = DVec3::ZERO;
 
-        if let Ok((drag, transform, thrust)) = player.single() {
-            player_drag = drag.vector();
+        if let Ok((angular_velocity, drag, force, transform, thrust, torque, velocity)) = 
+            player.single()
+        {
             player_position = transform.translation();
             player_thrust = thrust.vector();
+            player_drag = drag.vector();
+            player_force = force.vector();
+            player_torque = torque.vector();
+            player_velocity = velocity.vector();
+            player_angular_velocity = angular_velocity.vector();
         }
         if let Ok((impulse, impulsive_torque)) = player_optional.single() {
             player_impulse = impulse.0;
             player_impulsive_torque = impulsive_torque.0;
         };
+
+        info!("player position = {}", player_position);
+        info!("player thrust = {}", player_thrust);
+        info!("player drag = {}", player_drag);
+        info!("player force = {}", player_force);
+        info!("player position = {}", player_torque);
+        info!("player velocity = {}", player_velocity);
+        info!("player angular velocity = {}", player_angular_velocity);
+        info!("player impulse = {}", player_impulse);
+        info!("player impulsive_torque = {}", player_impulsive_torque);
 
         text.sections[1].value = format!("[{:.2}, {:.2}, {:.2}]",
                                          player_position.x, player_position.y, player_position.z);
@@ -153,10 +182,24 @@ fn update_player_debug_text(
                                          player_drag.x, player_drag.y, player_drag.z);
 
         text.sections[7].value = format!("[{:.2}, {:.2}, {:.2}]",
-                                         player_impulse.x, player_impulse.y, player_impulse.z);
+                                         player_force.x, player_force.y, player_force.z);
 
         text.sections[9].value = format!("[{:.2}, {:.2}, {:.2}]",
-                                         player_impulsive_torque.x, player_impulsive_torque.y, player_impulsive_torque.z);
+                                         player_torque.x, player_torque.y, player_torque.z);
+
+        text.sections[11].value = format!("[{:.2}, {:.2}, {:.2}]",
+                                         player_velocity.x, player_velocity.y, player_velocity.z);
+
+        text.sections[13].value = format!("[{:.2}, {:.2}, {:.2}]",
+                                         player_angular_velocity.x, player_angular_velocity.y,
+                                         player_angular_velocity.z);
+
+        text.sections[15].value = format!("[{:.2}, {:.2}, {:.2}]",
+                                         player_impulse.x, player_impulse.y, player_impulse.z);
+
+        text.sections[17].value = format!("[{:.2}, {:.2}, {:.2}]",
+                                         player_impulsive_torque.x, player_impulsive_torque.y,
+                                         player_impulsive_torque.z);
     }
 }
 
