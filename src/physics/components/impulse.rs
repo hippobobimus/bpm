38d85e76a7493944210c6a0e6f153caa68553b2a
Rob Masters <rjcmasters@gmail.com>
2021-11-09 14:41:19 +0000

    components for impulse and impulsive torque deprecated

--- src/physics/components/impulse.rs
+++
@@ -1,4 +0,0 @@
-use bevy::math::DVec3;
-
-/// A component that holds an impulse to be applied to an Entity.
-pub struct Impulse(pub DVec3);
--- src/physics/components/impulsive_torque.rs
+++
@@ -1,4 +0,0 @@
-use bevy::math::DVec3;
-
-/// A component that holds an impulsive torque to be applied to an Entity.
-pub struct ImpulsiveTorque(pub DVec3);
