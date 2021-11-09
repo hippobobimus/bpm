9db42f7f662ee91d7f5773789936d51d21c2b26a
Rob Masters <rjcmasters@gmail.com>
2021-11-09 16:33:46 +0000

    moved cache update and transform sync to separate system sets

--- src/physics/systems/integrator.rs
+++ src/physics/systems/integrator.rs
@@ -4,7 +4,6 @@ use crate::{
     constants,
     physics::components::{
         AngularVelocity,
-        BoundaryCollider,
         Force,
         InertiaTensor,
         Mass,
@@ -18,31 +17,20 @@ use crate::{
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
 enum IntegratorSystems {
     Integrate,
-    UpdateCache,
-    ResetAccumulators,
 }
 
 /// A SystemSet that runs the integrator to update velocity (including angular velocity) and
-/// transform components, subsequently updating any cached data that relies on the current
-/// transform and resetting the accumulators.
+/// transform components.
 pub fn get_system_set() -> SystemSet {
     SystemSet::new()
         .with_system(integrate.system()
                      .label(IntegratorSystems::Integrate)
         )
-        .with_system(update_cached_data.system()
-                     .label(IntegratorSystems::UpdateCache)
-                     .after(IntegratorSystems::Integrate)
-        )
-        .with_system(reset_accumulators.system()
-                     .label(IntegratorSystems::ResetAccumulators)
-                     .after(IntegratorSystems::Integrate)
-        )
 }
 
-/// An integration system that updates Velocity/AngularVelocity and PhysTransform/Transform
-/// components based on the attributes of the Entitys (Mass/InertiaTensor), the currently applied
-/// Force and Torque, and the timestep.
+/// An integration system that updates Velocity/AngularVelocity and PhysTransform components based
+/// on the attributes of the Entitys (Mass/InertiaTensor), the currently applied Force and Torque,
+/// and the timestep.
 fn integrate(
     time: Res<Time>,
     mut query: Query<(
@@ -52,15 +40,12 @@ fn integrate(
         &Mass,
         &mut PhysTransform,
         &Torque,
-        &mut Transform,
         &mut Velocity
     )>,
 ) {
     let dt_secs = time.delta_seconds_f64();
 
-    for (mut ang_v, f, inertia_tensor, m, mut transform, torque,
-         mut bevy_transform, mut v) in query.iter_mut()
-    {
+    for (mut ang_v, f, inertia_tensor, m, mut transform, torque, mut v) in query.iter_mut() {
         // Infinite mass objects cannot move.
         if m.is_infinite() { continue };
 
@@ -85,38 +70,9 @@ fn integrate(
             .normalize();
         transform.translation += v.vector() * dt_secs;
 
-        // Update bevy 32 bit float transform based on physics module's 64 bit transform.
-        bevy_transform.rotation = transform.rotation.as_f32();
-        bevy_transform.translation = transform.translation.as_f32();
-
         // If velocity is very low, make it 0.
         if v.vector().length_squared() < constants::LOW_VELOCITY_THRESHOLD {
             v.zero();
         }
-    }
-}
-
-/// Updates any cached derived data that relies on the PhysTransform, for Entitys that have moved.
-fn update_cached_data(
-    mut set: QuerySet<(
-        Query<(&mut PhysTransform, &mut InertiaTensor), Changed<PhysTransform>>,
-        Query<(&PhysTransform, &mut BoundaryCollider), Changed<PhysTransform>>,
-    )>,
-) {
-    for (mut transform, mut inertia_tensor) in set.q0_mut().iter_mut() {
-        transform.update();
-        inertia_tensor.update(transform.matrix());
-    }
-    for (transform, mut boundary) in set.q1_mut().iter_mut() {
-        // update the cached plane normal in the boundary if the boundary has moved.
-        boundary.0.update(transform);
-    }
-}
-
-/// A system that zeroes the Force and Torque accumulator components for all Entitys.
-fn reset_accumulators(mut query: Query<(&mut Force, &mut Torque)>) {
-    for (mut f, mut tq) in query.iter_mut() {
-        f.reset();
-        tq.reset();
     }
 }
