16e3b06ab249c491e03ae0b9d76f502b3cc380d8
Rob Masters <rjcmasters@gmail.com>
2021-11-09 11:49:27 +0000

    updated physics values to achieve desired interactions

--- src/constants.rs
+++ src/constants.rs
@@ -18,13 +18,16 @@ pub static DEFAULT_RADIUS: f64 = 1.0;
 pub static DAMPING_FACTOR: f64 = 0.999;
 pub static ANGULAR_DAMPING_FACTOR: f64 = 0.9;
 // --Drag
-pub static DEFAULT_K1: f64 = 5.0;
-pub static DEFAULT_K2: f64 = 5.0;
+pub static DEFAULT_K1: f64 = 0.1;
+pub static DEFAULT_K2: f64 = 0.1;
+// --Restitution
+pub static RESTITUTION_COEFF: f64 = 1.0;
 // --Float precision
-pub static LOW_VELOCITY_THRESHOLD: f64 = 1.0;
+pub static LOW_VELOCITY_THRESHOLD: f64 = 0.1;
+pub static LOW_ROTATION_THRESHOLD: f64 = 0.0001;
 // --Gravity
 lazy_static! {
-    pub static ref DEFAULT_GRAVITY: DVec3 = DVec3::new(0.0, 0.0, 0.0); // value of 'g'.
+    pub static ref DEFAULT_GRAVITY: DVec3 = DVec3::new(0.0, -10.0, 0.0); // value of 'g'.
 }
 // --Mass
 pub static DEFAULT_MASS: f64 = 10.0;
@@ -32,4 +35,6 @@ pub static DEFAULT_INVERSE_MASS: f64 = 0.1;
 // --Oct-Tree
 pub static MAX_OCT_TREE_DEPTH: i32 = 5;
 // --Thrust
-pub static DEFAULT_THRUST: f64 = 1000.0;
+pub static DEFAULT_THRUST: f64 = 50.0;
+// --Over-rotation limit
+pub static ANGULAR_LIMIT: f64 = 0.2;
