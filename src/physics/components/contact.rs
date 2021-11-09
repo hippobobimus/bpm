c2633e376db78987713c9e72e103d5ce31bd6899
Rob Masters <rjcmasters@gmail.com>
2021-11-09 11:58:55 +0000

    updated contact to hold the relative contact points on each body.

--- src/physics/components/contact.rs
+++ src/physics/components/contact.rs
@@ -5,11 +5,13 @@ use bevy::{
 
 #[derive(Debug)]
 /// Describes a contact between two separate bodies associated with two entities. Contains the two
-/// Entitys involved in the contact, the point of contact, amount of inter-penetration and the
-/// contact normal vector.
+/// Entitys involved in the contact (unless one of the Entitys is an immovable plane, in which case
+/// just the body that impacts the plane is referenced), the point of contact, amount of
+/// inter-penetration, the contact normal vector and the contact point(s) relative to each body.
 pub struct Contact {
-    pub entities: [Option<Entity>; 2],
+    pub entities: Vec<Entity>,
     pub normal: DVec3,
     pub penetration: f64,
     pub point: DVec3,
+    pub relative_points: Vec<DVec3>,
 }
