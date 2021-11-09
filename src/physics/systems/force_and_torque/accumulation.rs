9c47387683d37908bf0d678db0d845393d03f73f
Rob Masters <rjcmasters@gmail.com>
2021-11-09 16:21:26 +0000

    moved accumulator reset here

--- src/physics/systems/force_and_torque/accumulation.rs
+++ src/physics/systems/force_and_torque/accumulation.rs
@@ -18,13 +18,18 @@ use crate::physics::components::{
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
 enum ForceAndTorqueSystems {
     Accumulator,
+    Reset,
 }
 
 /// A SystemSet that resets and then recalculates the forces and torques applied on a body.
 pub fn get_system_set() -> SystemSet {
     SystemSet::new()
+        .with_system(reset_accumulators.system()
+                     .label(ForceAndTorqueSystems::Reset)
+        )
         .with_system(force_and_torque_accumulation.system()
                      .label(ForceAndTorqueSystems::Accumulator)
+                     .after(ForceAndTorqueSystems::Reset)
         )
 }
 
@@ -54,5 +59,13 @@ fn force_and_torque_accumulation(
     // Apply force and torque generators.
     for (rotator, mut f, mut torque, transform) in q.q3_mut().iter_mut() {
         rotator.update_force_and_torque(&mut f, &mut torque, transform);
+    }
+}
+
+/// A system that zeroes the Force and Torque accumulator components for all Entitys.
+fn reset_accumulators(mut query: Query<(&mut Force, &mut Torque)>) {
+    for (mut f, mut tq) in query.iter_mut() {
+        f.reset();
+        tq.reset();
     }
 }
